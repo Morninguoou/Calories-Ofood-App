6fedd23bb135bed1ef26d45376d79f7dc2444a43
Passakorn Polsan <65010833@kmitl.ac.th>
2024-09-21 11:19:41 +0700

    เอา branch ขึ้นเป็น public เฉยๆคับ

--- lib/screens/plannermainPage.dart
+++ lib/screens/plannermainPage.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class PlannerMain extends StatefulWidget {
@@ -10,6 +12,54 @@ class PlannerMain extends StatefulWidget {
 class _PlannerMainState extends State<PlannerMain> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFFF0D6B5), //F9F9F9
+      appBar: AppBar(
+        title: const Text('Meal Plan'),
+        backgroundColor: Color(0xFFF0D6B5),
+        titleTextStyle: TextStyle(
+          fontSize: 36.0,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                ),
+              ),
+              height: 656.0,
+              child: Expanded(
+                // Use Expanded to make the ListView take the remaining space
+                child: ListView.builder(
+                  itemCount: 9, // Number of items to display
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text('Task ${index + 1}'),
+                        subtitle: Text('Details of Task \n ${index + 1}'),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Function when button is pressed
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
