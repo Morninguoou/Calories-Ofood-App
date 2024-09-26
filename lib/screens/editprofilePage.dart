885b803544680c98df82b3b27490a605648e367e
Passakorn Polsan <65010833@kmitl.ac.th>
2024-09-26 21:50:16 +0700

    create plannerPage, eachplannerPage, profilePage, editprofilePage

---
+++ lib/screens/editprofilePage.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:projectapp/widget/widget_support.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFCF5EC),
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                height: 180.0,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 145.0,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 10, 31, 164),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20.0),
+                            bottomRight: Radius.circular(20.0),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                child: CircleAvatar(
+                  radius: 45.0,
+                  backgroundColor: Color(0xFF000000),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            //////////////////////////NAME////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Name', style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 30,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your name',
+                        counterText: '',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////EMAIL////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Email',
+                    style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 30,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your email(แก้ไม่ได้)',
+                        counterText: '',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////PASSWORD////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Password',
+                    style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 50,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter your password',
+                        counterText: '',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////BIO////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Bio', style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLines: 3,
+                      maxLength: 100,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////EMAIL////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Age', style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 3,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        counterText: '',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////HEIGHT////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Height',
+                    style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 3,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        counterText: '',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            //////////////////////////WEIGHT////////////////////////////
+            margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Weight',
+                    style: AppWidget.editprofiletitleTextFeildStyle()),
+                Container(
+                  height: 33.0,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, right: 5),
+                    child: TextField(
+                      maxLength: 3,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        counterText: "",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          GestureDetector(
+            child: Container(
+              width: 140.0,
+              height: 30,
+              decoration: BoxDecoration(
+                color: Color(0xFF4F6C4E),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Save Change',
+                    style: AppWidget.profilebuttonTextFeildStyle(),
+                  ),
+                ],
+              ),
+            ),
+            //onTap: {},
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/plannermainPage.dart
+++ lib/screens/plannermainPage.dart
@@ -1,6 +1,7 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:projectapp/widget/bottomnav.dart';
+import 'package:projectapp/screens/moredetailPage.dart';
+import 'package:projectapp/widget/widget_support.dart';
 
 class PlannerMain extends StatefulWidget {
   const PlannerMain({super.key});
@@ -13,10 +14,11 @@ class _PlannerMainState extends State<PlannerMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF0D6B5), //F9F9F9
+      backgroundColor: Color(0xFFF0D6B5), // Background color
       appBar: AppBar(
         title: const Text('Meal Plan'),
         backgroundColor: Color(0xFFF0D6B5),
+        elevation: 0.0,
         titleTextStyle: TextStyle(
           fontSize: 36.0,
           color: Colors.black,
@@ -29,22 +31,86 @@ class _PlannerMainState extends State<PlannerMain> {
             padding: const EdgeInsets.all(0.0),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: Color.fromARGB(255, 253, 253, 253),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(50.0),
                   topRight: Radius.circular(50.0),
                 ),
               ),
               height: 656.0,
-              child: Expanded(
-                // Use Expanded to make the ListView take the remaining space
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 child: ListView.builder(
-                  itemCount: 9, // Number of items to display
+                  itemCount: 7, // Number of items to display
                   itemBuilder: (context, index) {
-                    return Card(
-                      child: ListTile(
-                        title: Text('Task ${index + 1}'),
-                        subtitle: Text('Details of Task \n ${index + 1}'),
+                    return Container(
+                      margin: EdgeInsets.only(top: 20.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        //border: Border.all(color: Colors.black), // Added border
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Column(
+                              // Align the text to the top-left
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Plan ${index + 1}',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                    height: 10), // Adds spacing between texts
+                                Text(
+                                  'Details of Plan ${index + 1}.\n\n',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                // Ensure text can wrap properly
+                              ],
+                            ),
+                          ),
+                          // Positioned icon in the top-right corner
+                          Positioned(
+                            top: 5,
+                            right: 5,
+                            child: Icon(
+                              Icons.cancel,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Positioned(
+                            top: 10,
+                            right: 50,
+                            child: GestureDetector(
+                              child: Text(
+                                'More Detail',
+                                style: AppWidget.moredetailTextFeildStyle(),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Bottomnav(
+                                            initialPage: MoreDetailPlanner())));
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     );
                   },
