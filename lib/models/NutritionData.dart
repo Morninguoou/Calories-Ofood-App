aac0b4e5f739d0a1fd18140dc0c37a5aa3013faa
sirinya <sirinyasermsirisumpun@gmail.com>
2024-10-31 22:17:39 +0700

    Add new API and model files

---
+++ lib/models/NutritionData.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+class NutritionData {
+  String plannerID;
+  DateTime plannerDate;
+  String userID;
+  String planName;
+  DateTime createdAt;
+  int calories;
+  List<String> breakfast;
+  List<String> lunch;
+  List<String> dinner;
+  List<String> others;
+
+  NutritionData({
+    required this.plannerID,
+    required this.plannerDate,
+    required this.userID,
+    required this.planName,
+    required this.createdAt,
+    required this.calories,
+    required this.breakfast,
+    required this.lunch,
+    required this.dinner,
+    required this.others,
+  });
+
+  factory NutritionData.fromJson(Map<String, dynamic> json) {
+    return NutritionData(
+      plannerID: json['plannerID'],
+      plannerDate: DateTime.parse(json['plannerDate']),
+      userID: json['userID'] ?? '', // Handle null case if needed
+      planName: json['planname'] ?? '', // Handle null case if needed
+      createdAt: DateTime.parse(json['createdAt']),
+      calories: json['calories'],
+      breakfast: List<String>.from(json['breakfast'] ?? []),
+      lunch: List<String>.from(json['lunch'] ?? []),
+      dinner: List<String>.from(json['dinner'] ?? []),
+      others: List<String>.from(json['others'] ?? []),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'plannerID': plannerID,
+      'plannerDate': plannerDate.toIso8601String(),
+      'userID': userID,
+      'planname': planName,
+      'createdAt': createdAt.toIso8601String(),
+      'calories': calories,
+      'breakfast': breakfast,
+      'lunch': lunch,
+      'dinner': dinner,
+      'others': others,
+    };
+  }
+}
---
+++ lib/widget/selfplanlistforrecommen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:projectapp/models/mealplanPage.dart';
+import 'package:projectapp/widget/bonchonEdit_Box.dart';
+import 'package:projectapp/widget/widget_support.dart';
+
+class SelfPlanWidget extends StatelessWidget {
+  final List<Meal> meals;
+  final Function(bool) onEditModeChange;
+  //final VoidCallback confirmDishCountCallback;
+
+  const SelfPlanWidget({Key? key, required this.meals, required this.onEditModeChange,
+  //required this.confirmDishCountCallback
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: meals.map((meal) {
+        // Only show meals with foods
+        if (meal.foods.isNotEmpty) {
+          return Column(
+            children: [
+              buildMealBox(meal.meal, 'Total Calories: ${meal.totalCalories} cal', 
+              meal.foods.map((food) {
+                List<String> filteredTags = Food.fromJson(food.toJson()).toJson()['tags'];
+                  return BonchonBox(
+                    //key: bonchonBoxKey,
+                    foodName: food.name, // Assuming Food class has a 'name' property
+                    calories: food.calories, // Assuming Food class has a 'calories' property
+                    dish: food.dish,
+                    imageURL: food.imageURL,
+                    foodType: filteredTags,
+                    onEditModeChange: onEditModeChange,
+                    //confirmDishCountCallback: confirmDishCountCallback,
+                    foodID: food.foodID,
+                  );
+                }).toList()
+              ),
+              const SizedBox(height: 10.0),
+            ],
+          );
+        }
+        return SizedBox.shrink(); // Skip empty meals
+      }).toList(),
+    );
+  }
+
+  // Method to build a meal box
+  Widget buildMealBox(String mealType, String totalCalories, List<Widget> items) {
+    return Container(
+      ///////////////////////////////////// Box Widget
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: 10.0, left: 12.0, right: 12.0, bottom: 10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(mealType, style: AppWidget.dateboldTextFeildStyle()),
+                Text(totalCalories, style: AppWidget.totalcalTextFeildStyle()),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Column(children: items), // Add meal items here
+          ],
+        ),
+      ),
+    );
+  }
+}
